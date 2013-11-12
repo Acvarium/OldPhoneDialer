cd07ced01fe992e3141c577645e101df118b870e
acvarium <vshmorgun@yandex.ru>
2013-11-12 20:41:21 +0200

    Made change to layouts. Made view relative to screen resolution. Added action to central button and added animation to this button. Added button for write +, * and #. Changed action and text to button Clear.

--- src/com/example/oldphonedialer/MainActivity.java
+++ src/com/example/oldphonedialer/MainActivity.java
@@ -10,6 +10,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -22,17 +23,19 @@ import android.widget.TextView;
 @SuppressLint("NewApi")
 public class MainActivity extends Activity implements OnTouchListener,
 		OnClickListener {
-	private MediaPlayer aplo;
+	private MediaPlayer rotateS1, rotateS2;
 	float x, y;
-	ImageView imageView1, iv2;
-	TextView  tv2;
-	Button clear, call;
+	ImageView imageView1, iv2, iv3;
+	TextView tv2;
+	Button clear, plus, asterisk, gr;
 	float deg0, deltadeg, ivGetDeg, angle;
 	float pivotX, pivotY;
 	float deltaX, deltaY;
 	Typeface font;
 	String writeNumber;
 	RotateAnimation anim;
+	boolean newtouch;
+	float mainButtonRadius;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -42,63 +45,77 @@ public class MainActivity extends Activity implements OnTouchListener,
 		imageView1 = (ImageView) findViewById(R.id.imageView1);
 		tv2 = (TextView) findViewById(R.id.tv2);
 		iv2 = (ImageView) findViewById(R.id.iv2);
-		call = (Button) findViewById(R.id.call);
+		iv3 = (ImageView) findViewById(R.id.iv3);
+		plus = (Button) findViewById(R.id.plus);
+		asterisk = (Button) findViewById(R.id.asterisk);
+		plus = (Button) findViewById(R.id.plus);
+		gr = (Button) findViewById(R.id.gr);
 		clear = (Button) findViewById(R.id.clear);
+
 		writeNumber = "";
 		tv2.setText(writeNumber);
+
 		iv2.setOnTouchListener(this);
-		call.setOnClickListener(this);
+		asterisk.setOnClickListener(this);
 		clear.setOnClickListener(this);
-		font = Typeface.createFromAsset(getAssets(), "fonts/QumpellkaNo12.otf");  
+		plus.setOnClickListener(this);
+		asterisk.setOnClickListener(this);
+		gr.setOnClickListener(this);
+
+		font = Typeface.createFromAsset(getAssets(), "fonts/QumpellkaNo12.otf");
 		tv2.setTypeface(font);
-		clear.setTypeface(font);
-		call.setTypeface(font);
 	}
 
-	
-	
-	
-	private int angleToNum(float angle) {
+	public String removeLastChar(String s) {
+	    if (s == null || s.length() == 0) {
+	        return s;
+	    }
+	    return s.substring(0, s.length()-1);
+	}
+
+
+
+	private String angleToNum(float angle) {
 		// ----- 1
-		int a = 12;
+		String a = "";
 		if ((angle > 70) && (angle < 98)) {
-			a = 1;
+			a = "1";
 		}
 		// ----- 2
 		if ((angle > 98) && (angle < 120)) {
-			a = 2;
+			a = "2";
 		}
 		// ----- 3
 		if ((angle > 120) && (angle < 150)) {
-			a = 3;
+			a = "3";
 		}
 		// ----- 4
 		if ((angle > 150) && (angle < 177)) {
-			a = 4;
+			a = "4";
 		}
 		// ----- 5
 		if ((angle > 177) && (angle < 204)) {
-			a = 5;
+			a = "5";
 		}
 		// ----- 6
 		if ((angle > 204) && (angle < 230)) {
-			a = 6;
+			a = "6";
 		}
 		// ----- 7
 		if ((angle > 230) && (angle < 258)) {
-			a = 7;
+			a = "7";
 		}
 		// ----- 8
 		if ((angle > 258) && (angle < 285)) {
-			a = 8;
+			a = "8";
 		}
 		// ----- 9
 		if ((angle > 285) && (angle < 312)) {
-			a = 9;
+			a = "9";
 		}
 		// ----- 0
 		if (angle > 312) {
-			a = 0;
+			a = "0";
 		}
 		return a;
 	}
@@ -110,6 +127,15 @@ public class MainActivity extends Activity implements OnTouchListener,
 		return true;
 	}
 
+	private boolean pointInPosition(float x, float y, float x1, float y1,
+			float r) {
+		if ((Math.pow((x - x1), 2) + Math.pow((y - y1), 2)) <= Math.pow(r, 2)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
 		x = event.getX();
@@ -119,53 +145,69 @@ public class MainActivity extends Activity implements OnTouchListener,
 		deltaX = x - (iv2.getWidth() / 2);
 		deltaY = y - (iv2.getHeight() / 2);
 
+		mainButtonRadius = pivotX / 2.3f;
+
 		imageView1.setPivotX(pivotX);
 		imageView1.setPivotY(pivotY);
 
 		switch (event.getAction()) {
 		case MotionEvent.ACTION_DOWN: // нажатие
-			deg0 = (float) (Math.atan2(deltaX, deltaY) * (180 / Math.PI)) + 180;
-			ivGetDeg = imageView1.getRotation();
-			aplo = MediaPlayer.create(this, R.raw.frontsound);
-			aplo.setLooping(false);
-			aplo.start();
+
+			if (pointInPosition(x, y, pivotX, pivotY, mainButtonRadius)) {
+				iv3.setVisibility(View.VISIBLE);
+				newtouch = false;
+			} else {
+				iv3.setVisibility(View.INVISIBLE);
+				newtouch = true;
+				deg0 = (float) (Math.atan2(deltaX, deltaY) * (180 / Math.PI)) + 180;
+				ivGetDeg = imageView1.getRotation();
+				rotateS1 = MediaPlayer.create(this, R.raw.frontsound);
+				rotateS1.setLooping(false);
+				rotateS1.start();
+			}
+
 			break;
 		case MotionEvent.ACTION_MOVE: // движение
-			imageView1.setAnimation(null);
-			
-			angle = (float) (Math.atan2(deltaX, deltaY) * (180 / Math.PI)) + 180;
 
-			if ((imageView1.getRotation() <= 325)
-					|| (imageView1.getRotation() > 350)) {
-				deltadeg = deg0 - angle;
-				if (deltadeg < 0) {
-					deltadeg = 360 + deltadeg;
+			if (newtouch) {
+				imageView1.setAnimation(null);
+				angle = (float) (Math.atan2(deltaX, deltaY) * (180 / Math.PI)) + 180;
+
+				if ((imageView1.getRotation() <= 325)
+						|| (imageView1.getRotation() > 350)) {
+					deltadeg = deg0 - angle;
+					if (deltadeg < 0) {
+						deltadeg = 360 + deltadeg;
+					}
+					imageView1.setRotation(ivGetDeg + deltadeg);
 				}
-				imageView1.setRotation(ivGetDeg + deltadeg);
 			}
 
 			break;
 		case MotionEvent.ACTION_UP: // отпускание
+			iv3.setVisibility(View.INVISIBLE);
+			// iv2.setImageResource(R.drawable.d);
+			if (newtouch) {
+				if (imageView1.getRotation() < 346) {
+					anim = new RotateAnimation(imageView1.getRotation(), -0f,
+							pivotX, pivotY);
+					rotateS2 = MediaPlayer.create(this, R.raw.backsound);
+					rotateS2.setLooping(false);
+					rotateS2.seekTo(1150 - (int) Math.abs(deltadeg * 4));
+					rotateS2.start();
 
-			if (imageView1.getRotation() < 346) {
-				anim = new RotateAnimation(imageView1.getRotation(), -0f,
-						pivotX, pivotY);
-				aplo.stop();
-				aplo = MediaPlayer.create(this, R.raw.backsound);
-				aplo.setLooping(false);
-				aplo.seekTo(1150- (int) Math.abs(deltadeg * 4) );
-				aplo.start();
-				int num = angleToNum(imageView1.getRotation());
-				if (num < 12) {
-					writeNumber = writeNumber + String.valueOf(num);
+					writeNumber = writeNumber
+							+ angleToNum(imageView1.getRotation());
 					tv2.setText(writeNumber);
-				}
 
-				imageView1.setRotation(0);
-				anim.setDuration((long) Math.abs(deltadeg * 4));
-				imageView1.startAnimation(anim);
+					imageView1.setRotation(0);
+					anim.setDuration((long) Math.abs(deltadeg * 4));
+					imageView1.startAnimation(anim);
+				} else {
+					imageView1.setRotation(0);
+				}
 			} else {
-				imageView1.setRotation(0);
+				call();
 			}
 		case MotionEvent.ACTION_CANCEL:
 			break;
@@ -175,6 +217,7 @@ public class MainActivity extends Activity implements OnTouchListener,
 		return true;
 	}
 
+	// Made phone call
 	private void call() {
 		try {
 			Intent callIntent = new Intent(Intent.ACTION_CALL);
@@ -185,14 +228,44 @@ public class MainActivity extends Activity implements OnTouchListener,
 		}
 	}
 
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		Intent intent;
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			intent = new Intent(this, SetingsActivity.class);
+			startActivity(intent);
+			break;
+		case R.id.about:
+			intent = new Intent(this, About.class);
+			startActivity(intent);
+			break;
+
+		default:
+			break;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	// Processing button presses
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
-		case R.id.call:
-			call();
-			break;
 		case R.id.clear:
-			writeNumber = "";
+			writeNumber = removeLastChar(writeNumber);
+			tv2.setText(writeNumber);
+			break;
+		case R.id.asterisk:
+			writeNumber = writeNumber + "*";
+			tv2.setText(writeNumber);
+			break;
+		case R.id.gr:
+			writeNumber = writeNumber + "#";
+			tv2.setText(writeNumber);
+			break;
+		case R.id.plus:
+			writeNumber = writeNumber + "+";
 			tv2.setText(writeNumber);
 			break;
 		default:
---
+++ src/com/example/oldphonedialer/SetingsActivity.java
@@ -0,0 +1,12 @@
+package com.example.oldphonedialer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SetingsActivity extends Activity {
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.setings);
+	}
+}
